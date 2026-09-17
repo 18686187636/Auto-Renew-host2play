dd42847a39d1927619ad50e9b7e88936fdf3cce2
深蓝 <46840649+18686187636@users.noreply.github.com>
2026-09-17 17:15:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except ImportError:
 
 # ==================== 配置区域 ====================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6e26f506-ddf9-447b-847f-49e9f0879827",
+    "https://host2play.gratis/server/renew?i=db512ed6-3530-4c01-b74f-65a93966e14d",
     # 添加更多链接
 ]
 
